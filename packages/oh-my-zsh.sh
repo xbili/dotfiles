7e00465075cda7f6fa4960c898afd4c4710f4a73
Bili Xu <xubili93@gmail.com>
2018-01-02 10:58:29 +0800

    Include oh-my-zsh installation in setup scripts

--- packages/oh-my-zsh.sh
+++ packages/oh-my-zsh.sh
@@ -1,1 +1,5 @@
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Install oceanic-next theme
 git clone https://github.com/mhartington/oceanic-next-shell.git $HOME/.config/oceanic-next-shell
