1c99862f0009dacfdd8190510609152717f95126
Bili Xu <xubili93@gmail.com>
2018-06-10 17:13:36 +0800

    Fix broken symlink logic

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ echo "HOME: $HOME"
 # Symlinks all the dotfiles into your home directory
 echo "Setting up symlinks to custom dotfiles"
 
-for FILE in `ls -A ./dotfiles/`; do
+for FILE in `ls -A $PWD/dotfiles/`; do
     if [ -e "$HOME/$FILE" ]
     then
         echo "$HOME/$FILE already exists, renaming it to $FILE.bak"
@@ -17,6 +17,6 @@ for FILE in `ls -A ./dotfiles/`; do
     fi
 
     echo "Linking $FILE"
-    ln "./dotfiles/$FILE" "$HOME/$FILE";
+    ln -s "$PWD/dotfiles/$FILE" "$HOME/$FILE";
 done
 echo "Success!"
